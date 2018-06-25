ab956378e5309353ae257f3084eadb94e158910a
Irandir Onofre <irandironofre5@gmail.com>
2018-06-25 15:33:04 -0300

    fim

--- src/testes/TesteCDIO.java
+++
@@ -1,8 +0,0 @@
-package testes;
-
-public class TesteCDIO {
-	public static void main(String[] args) {
-		ExportExcel c = new ExportExcel();
-		c.expExcel("C:\\Users\\Irandir\\Desktop\\djkjhhjh.xls");
-	}
-}
